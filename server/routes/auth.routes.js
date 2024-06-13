ffdb5081e0fa905e1a87ea205d352683e7835b75
Ukupina <urkupinaapp@gmial.com>
2024-06-13 11:31:26 -0300

    mas cambios

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,6 +3,10 @@ const Authrouter = express.Router();
 
 const {login,register} = require('../controllers/uath.controllers.js')
 
+Authrouter.get("/",(req,res)=>{
+    res.send("funciona")
+})
+
 Authrouter.post("/login",login)
 
 Authrouter.post("/register",register)

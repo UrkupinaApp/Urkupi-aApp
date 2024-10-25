8228f13f5598154bc9343a1eaa31c9b0f8ce3f19
LucasGiambelluca <lucasdavigiambelluca@gmail.com>
2024-10-25 15:24:08 -0300

    back 123123123

--- server/controllers/ticket.controllers.js
+++ server/controllers/ticket.controllers.js
@@ -23,7 +23,7 @@ const GetTickets = (req,res)=>{
     })
 }
 
-const PostTicketCortesia = (req, res) => {
+/* const PostTicketCortesia = (req, res) => {
     const { N_ticket, dia, hora, qr_code, baño, caja, precio, carga } = req.body;
 
     // Validación de los datos de entrada
@@ -71,6 +71,63 @@ const PostTicketCortesia = (req, res) => {
         }
     );
 };
+ */
+
+
+
+const PostTicketCortesia = (req, res) => {
+    const { N_ticket, dia, hora = null, qr_code, baño = null, caja, precio, carga } = req.body;
+
+    // Log inicial para revisar el contenido de req.body
+    console.log('Datos de solicitud recibidos:', req.body);
+
+    // Validación de los datos de entrada
+    if (!N_ticket || !dia || !qr_code || !caja || precio === undefined) {
+        console.error('Faltan campos en la solicitud:', req.body);
+        return res.status(400).send({ message: "Faltan campos requeridos en la solicitud." });
+    }
+
+    let connect;
+    try {
+        connect = conectarDB();
+    } catch (connectionError) {
+        console.error('Error al conectar a la base de datos:', connectionError);
+        return res.status(500).send({ message: "Error al conectar a la base de datos.", error: connectionError });
+    }
+
+    // Definir el valor de 'carga' como true por defecto si no se proporciona en la solicitud
+    const cargaValor = carga !== undefined ? carga : true;
+
+    console.log(`Insertando ticket con N_ticket: ${N_ticket}, carga: ${cargaValor}`);
+
+    // Inserción en la tabla ticketscortesia
+    const insertQuery = `
+        INSERT INTO ticketscortesia 
+            (N_ticket, dia, hora, qr_code, baño, caja, carga, precio) 
+        VALUES 
+            (?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    connect.query(
+        insertQuery,
+        [N_ticket, dia, hora, qr_code, baño, caja, cargaValor, precio],
+        (err, result) => {
+            if (err) {
+                console.error('Error al insertar el ticket de cortesía:', err);
+                connect.end(); // Cierra la conexión antes de retornar
+                return res.status(500).send({ message: "Error al insertar el ticket de cortesía", error: err });
+            } else {
+                console.log('Ticket de cortesía insertado correctamente:', result);
+                connect.end(); // Cierra la conexión antes de retornar
+                return res.status(200).send({ message: "Ticket de cortesía cargado correctamente" });
+            }
+        }
+    );
+};
+
+
+
+
 
 const GetTicketsCortesia = (req, res) => {
   let connect = conectarDB();
@@ -232,116 +289,92 @@ const GetTicketsCortesia = (req, res) => {
 
 
 const reformatearNumeroTicket = (numero_ticket) => {
-  // Verificar si el número de ticket contiene comillas simples
-  if (/['"]/g.test(numero_ticket)) {
-      // Reemplazar las comillas simples por guiones
-      return numero_ticket.replace(/'/g, '-');
-  }
-  // Si no contiene comillas simples, retornar el ticket sin cambios
-  return numero_ticket;
+    return /['"]/g.test(numero_ticket) ? numero_ticket.replace(/'/g, '-') : numero_ticket;
 };
 
 const VerificarYActualizarTicket = (req, res) => {
     let { numero_ticket } = req.body;
-    
+
+    // Log inicial para verificar la entrada
+    console.log(`Solicitud recibida para verificar y actualizar el ticket: ${numero_ticket}`);
+
     // Verificar y reformatear el número de ticket si es necesario
     numero_ticket = reformatearNumeroTicket(numero_ticket);
-    
+
     // Validar el número de ticket después de reformatear
     if (!numero_ticket || typeof numero_ticket !== 'string') {
         console.error(`Número de ticket inválido después de reformatear: ${numero_ticket}`);
-        res.status(400).send("Número de ticket inválido después de reformatear.");
-        return;
+        return res.status(400).send("Número de ticket inválido después de reformatear.");
     }
-    
-    console.log(`Número de ticket reformateado para buscar: ${numero_ticket}`);
-    
+
     let connect;
     try {
         connect = conectarDB();
     } catch (connectionError) {
         console.error(`Error al conectar a la base de datos: ${connectionError}`);
-        res.status(500).send("Error al conectar a la base de datos.");
-        return;
+        return res.status(500).send("Error al conectar a la base de datos.");
     }
 
-    // Verifica si el ticket comienza con "Cortesia" o una letra mayúscula
-    let tabla = "";
-    if (numero_ticket.startsWith('Cortesia')) {
-        tabla = "ticketscortesia";
-    } else if (/^[A-Z]/.test(numero_ticket)) { // Comprueba si empieza con una letra mayúscula
-        tabla = "tickets";
-    } else {
+    // Determinar la tabla a consultar
+    const tabla = numero_ticket.startsWith('Cortesia') ? "ticketscortesia" :
+                  /^[A-Z]/.test(numero_ticket) ? "tickets" : null;
+
+    if (!tabla) {
         console.error(`Formato de número de ticket no válido: ${numero_ticket}`);
-        res.status(400).send("Formato de número de ticket no válido.");
         connect.end();
-        return;
+        return res.status(400).send("Formato de número de ticket no válido.");
     }
-    
+
     console.log(`Tabla seleccionada: ${tabla}`);
 
     // Consulta la tabla correspondiente
     const querySelect = `SELECT carga FROM ${tabla} WHERE N_ticket = ?`;
     console.log(`Ejecutando consulta: ${querySelect} con número de ticket: ${numero_ticket}`);
-    
+
     connect.query(querySelect, [numero_ticket], (err, results) => {
         if (err) {
             console.error(`Error al buscar el ticket en ${tabla}: ${err}`);
-            res.status(500).send("Error al buscar el ticket en la base de datos.");
             connect.end();
-            return;
+            return res.status(500).send("Error al buscar el ticket en la base de datos.");
         }
-
-        console.log(`Resultados de la consulta SELECT:`, results);
 
         if (results.length > 0) {
             const carga = results[0].carga;
-            console.log(`Valor de 'carga' obtenido: ${carga}`);
 
-            // Verifica que 'carga' no sea null o undefined
-            if (carga !== null && carga !== undefined) {
-                // Asegúrate de que 'carga' es una cadena antes de llamar a toLowerCase
-                const cargaStr = String(carga).toLowerCase();
-                console.log(`Valor de 'carga' convertido a minúsculas: ${cargaStr}`);
+            if (carga === true) {
+                // Obtener la fecha y hora actual para la descarga
+                const fechaActual = new Date();
+                const horaDescarga = fechaActual.toTimeString().split(' ')[0]; // Formato HH:MM:SS
+                const dateDescarga = fechaActual.toISOString().split('T')[0]; // Formato YYYY-MM-DD
 
-                if (cargaStr === 'true') {
-                    // Obtener la fecha y hora actual para la descarga
-                    const fechaActual = new Date();
-                    const horaDescarga = fechaActual.toTimeString().split(' ')[0]; // Formato HH:MM:SS
-                    const dateDescarga = fechaActual.toISOString().split('T')[0]; // Formato YYYY-MM-DD
+                console.log(`Actualizando el ticket con carga='false', hora_descarga='${horaDescarga}', date_descarga='${dateDescarga}'`);
 
-                    console.log(`Actualizando el ticket con carga='false', hora_descarga='${horaDescarga}', date_descarga='${dateDescarga}'`);
-
-                    // Actualizar el ticket con la carga, hora y fecha de descarga
-                    const queryUpdate = `UPDATE ${tabla} SET carga = ?, hora_descarga = ?, date_descarga = ? WHERE N_ticket = ?`;
-                    connect.query(
-                        queryUpdate, 
-                        ['false', horaDescarga, dateDescarga, numero_ticket], 
-                        (err, result) => {
-                            if (err) {
-                                console.error(`Error al actualizar el ticket en ${tabla}: ${err}`);
-                                res.status(500).send("Error al actualizar el ticket en la base de datos.");
-                            } else {
-                                console.log(`Ticket actualizado correctamente para el número de ticket: ${numero_ticket}`);
-                                res.status(200).send("Ticket actualizado y autorizado correctamente.");
-                            }
+                // Actualizar el ticket
+                const queryUpdate = `UPDATE ${tabla} SET carga = ?, hora_descarga = ?, date_descarga = ? WHERE N_ticket = ?`;
+                connect.query(
+                    queryUpdate, 
+                    ['false', horaDescarga, dateDescarga, numero_ticket], 
+                    (err, result) => {
+                        if (err) {
+                            console.error(`Error al actualizar el ticket en ${tabla}: ${err}`);
                             connect.end();
+                            return res.status(500).send("Error al actualizar el ticket en la base de datos.");
+                        } else {
+                            console.log(`Ticket actualizado correctamente para el número de ticket: ${numero_ticket}`);
+                            connect.end();
+                            return res.status(200).send("Ticket actualizado y autorizado correctamente.");
                         }
-                    );
-                } else {
-                    console.warn(`No autorizado: carga es '${cargaStr}' para el ticket ${numero_ticket}.`);
-                    res.status(403).send("No autorizado: carga no está en estado 'true'.");
-                    connect.end();
-                }
+                    }
+                );
             } else {
-                console.error(`El campo 'carga' es null o undefined para el ticket ${numero_ticket} en la tabla ${tabla}.`);
-                res.status(500).send("Datos incompletos: 'carga' no está definido.");
+                console.warn(`No autorizado: carga es '${carga}' para el ticket ${numero_ticket}.`);
                 connect.end();
+                return res.status(403).send("No autorizado: carga no está en estado 'true'.");
             }
         } else {
             console.warn(`No se encontró un ticket con el número ${numero_ticket} en ${tabla}.`);
-            res.status(404).send(`No se encontró un ticket con el número ${numero_ticket}.`);
             connect.end();
+            return res.status(404).send(`No se encontró un ticket con el número ${numero_ticket}.`);
         }
     });
 };

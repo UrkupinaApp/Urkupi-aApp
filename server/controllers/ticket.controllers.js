40b97ed7f9b6ca497a11647901f347a2302627b9
LucasGiambelluca <lucasdavigiambelluca@gmail.com>
2024-10-28 13:55:45 -0300

    back 1239989'

--- server/controllers/ticket.controllers.js
+++ server/controllers/ticket.controllers.js
@@ -273,7 +273,7 @@ const VerificarYActualizarTicket = (req, res) => {
             connect.end();
             return res.status(500).send("Error al buscar el ticket en la base de datos.");
         }
-
+        console.log(results)
         if (results.length > 0) {
             // Convertir el valor de carga a booleano
             const carga = results[0].carga === 'true';

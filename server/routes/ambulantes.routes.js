2bfae647a84453f4d1126474cb6f3e8b5fdf8197
LucasGiambelluca <lucasdavigiambelluca@gmail.com>
2024-11-15 14:22:40 -0300

    subida de ruta para carga de iamgenes

--- server/routes/ambulantes.routes.js
+++ server/routes/ambulantes.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { insertVendedor } = require('../controllers/ambulantesController.js');
-const router = express.Router();
+const ambulantesRoute = express.Router();
 
 // Ruta para registrar un vendedor ambulante
 router.post('/', insertVendedor);
 
-module.exports = router;
+module.exports = ambulantesRoute;
